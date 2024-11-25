7f41b2d564fab5ffbd4583c006ac339c9c34f98a
saeed <sk7126506@gmail.com>
2024-11-25 11:40:10 +0100

    l

--- smartome.py
+++ smartome.py
@@ -1,1 +1,7 @@
-print("hellow work")
+import pandas as pd
+
+# Use the on_bad_lines argument to skip bad lines
+data = pd.read_csv('Export_Conso_Tours Habitat_2024__2024-05-09_16h11.csv', on_bad_lines='skip')
+
+# Check the first few rows of the data
+print(data.head())
